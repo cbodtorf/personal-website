b72f5ed03ed00fe92ddeeb7d84b84a0e7a621853
Cbodtorf <cbodtorf@gmail.com>
2016-08-22 15:32:55 -0400

    added social icons, chart.js for tech skills, changed the image sizes to reduced project size from ~8mb -> ~2mb, changed image loading in the browser module (toggles classes for instant access rather than dynamically loading images) **significant performance tweak.

--- main.js
+++ main.js
@@ -30,7 +30,43 @@
     * STYLES
     ********************************/
     styles: function() {
+      var ctx = document.getElementById("myChart");
 
+      var myChart = new Chart(ctx, {
+          type: 'radar',
+          data: {
+              labels: ["HTML", "ANGULAR", "CSS", "SASS", "JAVASCRIPT", "NODE", "JQUERY", "GULP"],
+              datasets: [
+                {
+                    label: "Mastery",
+                    backgroundColor: "rgba(144, 255, 194, 0.3)",
+                    borderColor: "rgba(144, 255, 194, 1)",
+                    pointBackgroundColor: "rgba(144, 255, 194, 1)",
+                    pointBorderColor: "#fff",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(144, 255, 194, 1)",
+                    data: [90, 75, 90, 80, 80, 55, 70, 80]
+                },
+                {
+                    label: "Experience",
+                    backgroundColor: "rgba(167, 107, 204, 0.3)",
+                    borderColor: "rgba(167, 107, 204, 1)",
+                    pointBackgroundColor: "rgba(167, 107, 204, 1)",
+                    pointBorderColor: "#fff",
+                    pointHoverBackgroundColor: "#fff",
+                    pointHoverBorderColor: "rgba(167, 107, 204, 1)",
+                    data: [80, 60, 80, 70, 75, 40, 50, 65]
+                }
+            ]
+          },
+          options: {
+                  scale: {
+                      ticks: {
+                          beginAtZero: true
+                      }
+                  }
+          }
+      });
     },
 
     /*******************************
@@ -42,22 +78,40 @@
       * Browser tab action [change img, url, tab focus]
       ********************************/
       var addy = document.getElementById('addressBar');
-      var webImg = document.getElementById('webImg');
+      var webImgs = Array.from(document.querySelectorAll('.web-img'));
       var tabs  = Array.from(document.querySelectorAll('.tab'));
 
       tabs.forEach(handleClick);
 
       function handleClick(e,i) {
-        e.addEventListener('click', function(){
+          e.addEventListener('click', function(){
 
-          tabs.forEach(function(e,i){
-            e.classList.remove('active');
+              // remove active from all tabs
+              //
+              tabs.forEach(function(el,i){
+                el.classList.remove('active');
+              })
+
+              // remove active from all images
+              //
+              webImgs.forEach(function(el,i){
+                  el.classList.remove('active-img');
+
+                  // matches clicked tab with proper image based on data-id
+                  // .active-img { display: block }
+                  //
+                  if(el.dataset.id === e.dataset.id) {
+                    el.classList.add('active-img');
+                  }
+              })
+
+              // changes address bar (anchor tag) href and textContent to match img
+              // .active { highlights active tab styling }
+              //
+              addy.textContent = e.dataset.id;
+              addy.href = e.dataset.id;
+              e.classList.add('active');
           })
-          webImg.src = e.dataset.img;
-          addy.value = e.dataset.id;
-          e.classList.add('active');
-          console.log(e);
-        })
       }
 
 
